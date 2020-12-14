cbd381d57567508ce1aa2377041399f0681e614b
biniam haile <biniamhk@gmail.com>
2020-12-14 11:49:29 +0100

    jenkins to show failed test is now fixed

--- src/test/java/se/labb2_junit/TestCalculator.java
+++ src/test/java/se/labb2_junit/TestCalculator.java
@@ -40,7 +40,7 @@ public class TestCalculator {
     @Test
     @DisplayName("testing subtraction")
     public void testSub() {
-        assertNotEquals(2, calculator.subtraction(5, 3));
+        assertNotEquals(3, calculator.subtraction(5, 3));
 
 
     }
